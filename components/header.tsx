25e0dc6418026acc3e8c75472a976025db4039dc
Darshan S <darshans2727@gmail.com>
2026-04-10 18:33:37 +0530

    feat: add icon to header link for project management

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link'
 import { ModeToggle } from './mode-toggle'
 import UserSignIn from './user-sign-in'
+import { HugeiconsIcon } from '@hugeicons/react'
+import { TimeManagementIcon } from '@hugeicons/core-free-icons'
 
 export default function Header() {
   return (
     <header className="h-12 w-full flex items-center justify-between px-4 bg-secondary text-secondary-foreground sticky z-10 top-0 left-0">
       <div className="header-left text-xs sm:text-sm">
-        <Link href={'/'}>Project management</Link>
+        <Link href={'/'} className="flex items-center gap-2">
+          <HugeiconsIcon icon={TimeManagementIcon} />
+          Project management
+        </Link>
       </div>
       <div className="header-right flex items-center gap-3">
         <UserSignIn />
